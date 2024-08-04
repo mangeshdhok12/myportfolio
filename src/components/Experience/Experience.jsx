3f789ae4ce756a9581e94b04d99cf6163a868c46
Mangesh Dhok <dhokmangesh678@gmail.com>
2024-08-04 11:54:37 +0530

    Add node_modules to .gitignore and remove it from the repository

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -31,7 +31,7 @@ export const Experience = () => {
                   alt={`${historyItem.organisation} Logo`}
                 />
                 <div className={styles.historyItemDetails}>
-                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                  <h2>{`${historyItem.role}, ${historyItem.organisation}`}</h2>
                   <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
                   <ul>
                     {historyItem.experiences.map((experience, id) => {
